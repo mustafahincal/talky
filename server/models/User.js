aafb829c04d736a461f6283390e7e3645401d818
mustafahincal <mustafahncal@gmail.com>
2023-01-30 20:33:58 +0300

    create Chat, Message, User models

---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        image: {
+            type: String,
+            required: true,
+            default:
+                "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+        }
+    },
+    {
+        timestamps: true
+    })
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
