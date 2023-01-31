8019e88286fbcc084c729e64719d4eb12d700df4
mustafahincal <mustafahncal@gmail.com>
2023-01-31 15:00:27 +0300

    add errorMiddleware

--- server/app.js
+++ server/app.js
@@ -3,19 +3,22 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const chats = require("./data/data");
 const userRouter = require("./routers/userRouter");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
+
+//* db
 connectDB();
 
+//* middlewares
+app.use(express.json());
+
+//* routes
 app.use("/api/user", userRouter);
 
-app.get("/", (req, res) => {
-  res.send("api is running");
-});
-app.get("/api/chat", (req, res) => {
-  res.json(chats);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`server started on ${PORT}`));
