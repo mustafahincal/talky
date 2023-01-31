584fda987587e8b837c1932fb039c9f661e60d9d
mustafahincal <mustafahncal@gmail.com>
2023-01-31 13:10:05 +0300

    add content to SignUp component

--- client/src/components/authentication/SignUp.tsx
+++ client/src/components/authentication/SignUp.tsx
@@ -1,7 +1,100 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [pic, setPic] = useState<string>("");
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClickShow = () => {
+    setShow((prev) => !prev);
+  };
+  const handleUploadImage = (img: File | null) => {
+    console.log(img);
+  };
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email Address</FormLabel>
+        <Input
+          type="email"
+          placeholder="Enter Your Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClickShow}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirmPassword" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </InputGroup>
+      </FormControl>
+      <FormControl id="pic">
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          onChange={(e) =>
+            handleUploadImage(e.target.files ? e.target.files[0] : null)
+          }
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={handleSubmit}
+        //isLoading={picLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
 };
 
 export default SignUp;
