5c39135f9a372fcfe146a1cb551b1b7647dfd89c
mustafahincal <mustafahncal@gmail.com>
2023-02-01 19:59:02 +0300

    add authMiddleware for bearer token

--- server/config/generateToken.js
+++ server/config/generateToken.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SCREET, { expiresIn: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
 module.exports = generateToken;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { register, authUser, getAll } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(register).get(getAll);
+router.route("/").post(register).get(protect, getAll);
 router.route("/login").post(authUser);
 
 module.exports = router;
