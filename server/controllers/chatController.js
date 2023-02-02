c9c64eb2956697783d25d95ad30f5d9802b5274e
mustafahincal <mustafahncal@gmail.com>
2023-02-02 18:54:20 +0300

    add renameGroup and addToGroup functions

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -129,10 +129,53 @@ const renameGroup = asyncHandler(async (req, res) => {
 });
 
 const removeFromGroup = asyncHandler(async (req, res) => {
-  console.log("accessChat");
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!removed) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(removed);
+  }
 });
+
 const addToGroup = asyncHandler(async (req, res) => {
-  console.log("accessChat");
+  const { chatId, userId } = req.body;
+
+  // check if the requester is admin
+
+  const added = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!added) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(added);
+  }
 });
 
 module.exports = {
