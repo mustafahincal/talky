976bda56ba7ab1a5ff02bf53bed4fecec5e37def
mustafahincal <mustafahncal@gmail.com>
2023-02-18 14:16:36 +0300

    install react-toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,37 @@ import HomePage from "./pages/HomePage";
 import ChatPage from "./pages/ChatPage";
 import { Chat } from "./types/chat";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const [chats, setChats] = useState<Chat[]>([]);
 
   /* const fetchChats = async () => {
-    const res = await axios.get("http://localhost:4000/api/chat");
+    const res = await axios.get(REACT_APP_BASE_ENDPOINT + "/chat");
     setChats(res.data);
   };
   useEffect(() => {
     fetchChats();
   }, []); */
   return (
-    <>
+    <div className="App">
       <HomePage />
       {/*  <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/chat" element={<ChatPage chats={chats} />} />
       </Routes> */}
-    </>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
   );
 }
 
