220bde888552ad8b4309817d1feadbe551485e0e
mustafahincal <mustafahncal@gmail.com>
2023-02-19 16:00:16 +0300

    add ChatPage components

---
+++ client/src/components/chat/Conversation.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Conversation = () => {
+  return <Container className="bg-danger">Conversation</Container>;
+};
+
+export default Conversation;
---
+++ client/src/components/chat/Users.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Users = () => {
+  return <Container className="bg-primary">Conversation</Container>;
+};
+
+export default Users;
