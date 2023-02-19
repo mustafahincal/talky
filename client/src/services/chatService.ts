3be3a61d8eedf8401a107992d24f93afcd0d6ff2
mustafahincal <mustafahncal@gmail.com>
2023-02-19 18:50:36 +0300

    add message type and fetchAllMessages function

--- client/src/services/chatService.ts
+++ client/src/services/chatService.ts
@@ -4,3 +4,5 @@ const url = process.env.REACT_APP_BASE_ENDPOINT + "/chat";
 
 export const fetchAllChats = () => get(url);
 export const fetchAccessChats = (userId: number) => post(url, { userId });
+export const fetchAllMessagesByChatId = (chatId: number) =>
+  get(url + "/" + chatId);
---
+++ client/src/types/message.ts
@@ -0,0 +1,7 @@
+import { User } from "./user";
+
+export type Message = {
+  _id: string;
+  sender: string;
+  chat: string;
+};
